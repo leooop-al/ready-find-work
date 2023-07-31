c935dbec8c2a6da65f49cc5141c0601cfb8b28eb
seuers_leo <389141994@qq.com>
2023-07-31 09:04:07 +0800

    add template

---
+++ leetcode/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author ziyou.cxf
+ * @version $Id: ${NAME}.java, v 0.1 ${YEAR}年${MONTH}月${DAY}日 ${TIME} ziyou.cxf Exp $
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
